2cccab0577f4444fc1ccc410ecbd0939863b4fd3
Feynman Liang <feynman.liang@gmail.com>
2016-05-17 23:31:16 +0100

    Removes Mapper and Nat for now

--- src/main/scala/shapely/package.scala
+++ src/main/scala/shapely/package.scala
@@ -10,10 +10,6 @@ package object shapely {
 
     def remove[A](implicit R: Remover[A, L]): R.Out =
       R(self)
-
-    def map[P <: Poly](p: P)(implicit M: Mapper[L, P]): M.Out = M(self)
-
-    def nth(n: Nat)(implicit N: Nther[L, n.N): N.Out = N(self)
   }
 }
 
